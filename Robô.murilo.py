06d0e66f32b697d67a92e870ca596ee251977f98
Murilosantana7 <murilo.santana@shopee.com>
2025-08-12 21:07:01 -0300

    Update Robô.murilo.py

--- Robô.murilo.py
+++ Robô.murilo.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import gspread
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-import pickle
+from google.oauth2.service_account import Credentials
 import os
 import requests
 import time
@@ -14,21 +12,11 @@ INTERVALO = 'C:H'
 WEBHOOK_URL = "https://openapi.seatalk.io/webhook/group/5KZq9RrWR5eEbMCzBoapOw"
 
 def autenticar_google():
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+    # Caminho para o arquivo JSON da service account, que será restaurado na pasta GOOGLE_CREDENTIALS
+    credentials_path = os.path.join('GOOGLE_CREDENTIALS', 'service_account.json')
     
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-            
+    creds = Credentials.from_service_account_file(credentials_path, scopes=SCOPES)
+    
     return creds
 
 def obter_totais_por_fanout(spreadsheet_id, nome_aba, intervalo):
